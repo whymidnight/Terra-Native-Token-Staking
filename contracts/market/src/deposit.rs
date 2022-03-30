d812d6bc80e29f45c7707550d15e4cacb3b31e36
Dominic DiGiacomo <ddigiacomo@triptychlabs.io>
2022-03-30 12:48:09 -0700

    Recalc on withdrawal

--- contracts/market/src/deposit.rs
+++ contracts/market/src/deposit.rs
@@ -11,16 +11,19 @@ use crate::state::{
 
 use cw20::Cw20ExecuteMsg;
 
+// TODO: CHANGE TO 24 HOURS
+const DURATION: u64 = 30;
+
 pub fn deposit_stable(
     deps: DepsMut,
     env: Env,
     info: MessageInfo,
 ) -> Result<Response, ContractError> {
+    let time = env.block.time.seconds();
     // we need to _un_account global accrued_interest_payments by previous interested_balance
     // to maintain congruency of how much to fund contract wallet bank since we must only add
     // to global accrued_interest_payments once per ident.
     let mut state: State = read_state(deps.storage)?;
-    let time = env.block.time.seconds();
     let config: Config = read_config(deps.storage)?;
     let ident = info.sender;
     let ident_raw = deps.api.addr_canonicalize(ident.as_str())?;
@@ -41,19 +44,15 @@ pub fn deposit_stable(
     if deposit.initial_interaction == 0 {
         deposit.initial_interaction = time;
     }
-    if deposit.last_interaction == 0 {
-        deposit.last_interaction = time;
-    }
     let last_interaction = deposit.last_interaction;
     let duration = time - last_interaction;
-    let days = duration / (30);
+    let days = duration / (DURATION);
     deposit.last_interaction = time;
 
     let mut accrued_interest = Uint128::zero();
-    let accrued_interest_dbg = deposit.interested_balance;
     if days > 0 {
         accrued_interest = calculate_accrued_interest(&config, &deposit, days)?;
-        state.accrued_interest_payments -= accrued_interest_dbg;
+        state.accrued_interest_payments -= deposit.interested_balance;
         state.accrued_interest_payments += accrued_interest;
     }
     deposit.balance += deposit_amount;
@@ -82,32 +81,50 @@ pub fn deposit_stable(
 
 pub fn redeem_stable(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     sender: Addr,
-    burn_amount: Uint128,
+    withdraw_amount: Uint128,
 ) -> Result<Response, ContractError> {
+    let time = env.block.time.seconds();
+    let mut state: State = read_state(deps.storage)?;
     let config: Config = read_config(deps.storage)?;
+    let ident = sender;
+    let ident_raw = deps.api.addr_canonicalize(ident.as_str())?;
 
+    let mut deposit = read_deposit_info(deps.storage, &ident_raw);
+    deposit.balance -= withdraw_amount;
+    let last_interaction = deposit.last_interaction;
+    let duration = time - last_interaction;
+    let days = duration / (DURATION);
+
+    let mut accrued_interest = Uint128::zero();
+    if days > 0 {
+        accrued_interest = calculate_accrued_interest(&config, &deposit, days)?;
+        state.accrued_interest_payments -= deposit.interested_balance;
+        state.accrued_interest_payments += accrued_interest;
+    }
+
+    deposit.interested_balance = accrued_interest;
+    deposit.last_interaction = time;
     Ok(Response::new()
         .add_messages(vec![
             CosmosMsg::Wasm(WasmMsg::Execute {
                 contract_addr: deps.api.addr_humanize(&config.aterra_contract)?.to_string(),
                 funds: vec![],
                 msg: to_binary(&Cw20ExecuteMsg::Burn {
-                    amount: burn_amount,
+                    amount: withdraw_amount,
                 })?,
             }),
             CosmosMsg::Bank(BankMsg::Send {
-                to_address: sender.to_string(),
+                to_address: ident.to_string(),
                 amount: vec![Coin {
                     denom: config.stable_denom,
-                    amount: Uint128::from(3 as u128).into(),
+                    amount: withdraw_amount,
                 }],
             }),
         ])
         .add_attributes(vec![
             attr("action", "redeem_stable"),
-            attr("burn_amount", burn_amount),
-            // attr("redeem_amount", redeem_amount),
+            attr("withdraw_amount", withdraw_amount),
         ]))
 }
